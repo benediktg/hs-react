d37d93a6eb13c81ea71b0cde7f25f1806f3c8f70
Benedikt Geissler <benedikt@g5r.eu>
2017-06-10 04:33:29 +0200

    Extract another component

--- react-demo/src/ChatInput.js
+++ react-demo/src/ChatInput.js
@@ -1,29 +1,19 @@
 import React from 'react';
+import LoginForm from './LoginForm';
 
 function ChatInput(props) {
   return (
     <div className="row">
       <div className="col-sm">
         <form onSubmit={props.onSubmit}>
-          {props.loggedIn
-            ? <div className="input-group fluid">
-                <p><strong>{props.username}</strong></p>
-                <button name="login" onClick={props.onLogin}>Logout</button>
-              </div>
-            : <div className="input-group fluid">
-                <input
-                  name="username"
-                  type="text"
-                  value={props.username}
-                  onChange={props.onUsernameInput}
-                  placeholder="Name"
-                  size="1"
-                />
-                <button name="login" onClick={props.onLogin}>Login</button>
-              </div>}
+          <LoginForm
+            loggedIn={props.loggedIn}
+            onLogin={props.onLogin}
+            username={props.username}
+            onUsernameInput={props.onUsernameInput}
+          />
           <div className="input-group fluid">
             <textarea
-              name="messageText"
               value={props.messageText}
               onChange={props.onMessageTextInput}
               disabled={!props.loggedIn}
