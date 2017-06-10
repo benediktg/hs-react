3a04a47f7c746767040b962d07e449e4c21acc45
Benedikt Geissler <benedikt@g5r.eu>
2017-06-10 03:58:20 +0200

    Add mini.css via yarn

--- react-demo/src/index.js
+++ react-demo/src/index.js
@@ -1,8 +1,8 @@
+import 'mini.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './style/mini-default.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
